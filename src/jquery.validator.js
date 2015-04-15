def632e5f5f707c9b32a5bad4b8c3ca7b3a3be23
Jony <zj86@live.cn>
2015-04-15 16:30:35 +0800

    Fixed #180

--- src/jquery.validator.js
+++ src/jquery.validator.js
@@ -125,7 +125,7 @@
         ignoreBlank   {Boolean}     false     When the field has no value, whether to ignore verification
         ignore        {jqSelector}    ''      Ignored fields (Using jQuery selector)
         
-        beforeSubmit  {Function}              Do something before submiting the form
+        beforeSubmit  {Function}              Do something before submit the form
         dataFilter    {Function}              Conversion ajax results
         valid         {Function}              Triggered when the form is valid
         invalid       {Function}              Triggered when the form is invalid
@@ -1059,7 +1059,7 @@
                     }
                 }
                 if (!$msgbox) {
-                    datafor = !checkable(el) && el.id ? el.id : el.name;
+                    datafor = (!checkable(el) || !el.name) && el.id ? el.id : el.name;
                     $msgbox = this.$el.find(msgOpt.wrapper + '.' + CLS_MSG_BOX + '[for="' + datafor + '"]');
                 }
             } else {
@@ -1622,11 +1622,16 @@
             var me = this,
                 elem, count;
 
-            count = me.$el.find('input[name="' + element.name + '"]').filter(function() {
-                var el = this;
-                if (!elem && checkable(el)) elem = el;
-                return !el.disabled && el.checked;
-            }).length;
+            if (element.name) {
+                count = me.$el.find('input[name="' + element.name + '"]').filter(function() {
+                    var el = this;
+                    if (!elem && checkable(el)) elem = el;
+                    return !el.disabled && el.checked;
+                }).length;
+            } else {
+                elem = element;
+                count = elem.checked;
+            }
 
             if (params) {
                 return me.getRangeMsg(count, params, 'checked');
