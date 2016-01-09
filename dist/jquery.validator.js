d03020e82d28da80af7c6f489a36fa0d70ed80cd
Jony Zhang <niceue@live.com>
2016-01-09 14:36:08 +0800

    build new banner

--- dist/jquery.validator.js
+++ dist/jquery.validator.js
@@ -1,6 +1,6 @@
-/*! nice Validator 0.10.0
- * (c) 2012-2015 Jony Zhang <niceue@live.com>, MIT Licensed
- * http://validator.niceue.com/
+/*! nice-validator 0.10.0
+ * (c) 2012-2016 Jony Zhang <niceue@live.com>, MIT Licensed
+ * https://github.com/niceue/nice-validator
  */
 /*jshint evil:true*/
 (function(factory) {
