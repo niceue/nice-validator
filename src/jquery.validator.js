5f58a09bc819f9668b8b3060b2321176cb682c1f
Jony Zhang <niceue@live.com>
2016-08-21 22:19:35 +0800

    Keep update isValid status

--- src/jquery.validator.js
+++ src/jquery.validator.js
@@ -812,17 +812,17 @@
             ret.id = el.id;
             ret.value = field.value;
 
+            me.elements[field.key] = ret.element = el;
+            me.isValid = me.$el[0].isValid = isValid ? me.isFormValid() : isValid;
+
             if (isValid) {
                 ret.type = 'ok';
             } else {
                 if (me.submiting) {
                     me.errors[field.key] = ret.msg;
                 }
-                me.isValid = false;
                 me.hasError = true;
             }
-            me.elements[field.key] = ret.element = el;
-            me.$el[0].isValid = isValid ? me.isFormValid() : isValid;
 
             // cache result
             field.old = ret;
